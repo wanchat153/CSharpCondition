aa00b8814858a6ae4b463c82d2678e36155a2575
M153 <wanchatgame@gmail.com>
2023-09-05 12:00:55 +0700

    And OR

--- CSharpCondition/Program.cs
+++ CSharpCondition/Program.cs
@@ -14,14 +14,25 @@ namespace CSharpCondition
             Console.WriteLine("Are you over 18?");
             Console.WriteLine("Enter Age :");
             int age = int.Parse(Console.ReadLine());
-            if (age < 18)
+            int salary = 15000;
+
+            //And = && OR = ||
+            if (age <= 18 && salary == 15000)
             {
-                Console.WriteLine("False");
-            }else if (age >= 18)
+                Console.WriteLine("You age under 18. Salary you 15000");
+            }
+            else if ((age >= 18 && salary == 15000) || (age >= 20 && salary == 20000))
             {
-                Console.WriteLine("True");
+                Console.WriteLine("You age over or 18 - 20. Salary you 15000 - 20000");
+            }
+            else if (age <= 0)
+            {
+                Console.WriteLine("Error");
             }
             Console.WriteLine("Bye");
+
+            //Input end
+            Console.ReadLine();
         }
     }
 }
